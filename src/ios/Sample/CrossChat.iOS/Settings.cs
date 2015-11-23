d9047faae35c3b00e373160f3a9adaac51505fa5
hdknr <gmail@hdknr.com>
2015-11-23 22:40:10 +0900

    Chat Settings

---
+++ src/ios/Sample/CrossChat.iOS/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+
+namespace CrossChat.iOS
+{
+	public class Settings
+	{
+		
+		protected NSDictionary _res = null;
+
+		public Settings(string path="Settings.bundle/Root.plist")
+		{
+			_res = new NSDictionary (
+				NSBundle.MainBundle.PathForResource (path, null)
+			);
+		}
+
+		protected Dictionary<string, string> _default_preference = null;
+
+		public Dictionary<string, string> DefaultPreference
+		{
+			get{
+				if (_default_preference == null) {
+					var prefs = _res ["PreferenceSpecifiers"] as NSArray;
+
+					_default_preference =  NSArray.FromArray<NSDictionary> (prefs)
+						.Where (i => i ["DefaultValue"] != null)
+						.ToDictionary (
+							k => k ["Key"].ToString (), v => v ["DefaultValue"].ToString ());
+				}
+				return _default_preference;
+			}
+		}
+
+		public string GetPreference(string key){
+			var ret = NSUserDefaults.StandardUserDefaults.StringForKey (key);
+			return ret ?? DefaultPreference [key];
+		}
+	}
+}
+
