786191b049195d5d110e9038beb339956b1e9480
hdknr <gmail@hdknr.com>
2015-11-23 23:01:56 +0900

    Subscript crosschar topic hosted by crossbar.io WAMP router

---
+++ src/ios/Sample/CrossChat.iOS/Topic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Reactive.Subjects;
+using WampSharp.V2;
+using WampSharp.V2.PubSub;
+using WampSharp.V2.Realm;
+using WampSharp.V2.Client;
+using WampSharp.V2.Core.Contracts;
+using WampSharp.V2.Rpc;
+
+using Newtonsoft.Json.Linq;
+
+namespace CrossChat.iOS
+{
+	public class Topic
+	{
+		ISubject<JObject> _subject= null;
+		IDisposable _subscription  = null;
+		IWampChannel _channel = null;
+
+		public Topic (Action<JObject> on_message)
+		{
+			var settings = new Settings ();
+
+			var channelFactory = new DefaultWampChannelFactory();
+			var auth = 	new WampCraClientAuthenticator(
+				authenticationId:  settings.GetPreference("chat_user"),
+				secret: settings.GetPreference("chat_token"));
+
+			_channel = channelFactory.CreateJsonChannel(
+				settings.GetPreference("chat_server"),
+				settings.GetPreference("chat_realm"),
+				auth);
+
+			_channel.RealmProxy.Monitor.ConnectionEstablished += (
+				object s, WampSessionCreatedEventArgs e) => {
+				var realmProxy = _channel.RealmProxy;
+
+				_subject = realmProxy.Services.GetSubject<JObject> (
+					settings.GetPreference ("chat_topic"));
+
+				_subscription = _subject.Subscribe (x => on_message (x));
+			};
+
+			_channel.Open().Wait();
+		}
+	}
+}
+
--- src/ios/Sample/CrossChat.iOS/ViewController.cs
+++ src/ios/Sample/CrossChat.iOS/ViewController.cs
@@ -2,10 +2,13 @@
 
 using UIKit;
 
+using Newtonsoft.Json.Linq;
+
 namespace CrossChat.iOS
 {
 	public partial class ViewController : UIViewController
 	{
+		Topic _topic =null;
 		public ViewController (IntPtr handle) : base (handle)
 		{
 		}
@@ -14,6 +17,9 @@ namespace CrossChat.iOS
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			_topic = new Topic (OnMessage);
+
 		}
 
 		public override void DidReceiveMemoryWarning ()
@@ -21,6 +27,10 @@ namespace CrossChat.iOS
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
 		}
+		public void OnMessage(JObject message){
+			Console.WriteLine("@  Nick {0} : {1}", message["nick"], message["message"]);
+		}
+
 	}
 }
 
